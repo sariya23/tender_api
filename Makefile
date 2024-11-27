a5312dddeb223c13a71377950d6638eca36fe571
sariya <dayzwaitik123@yandex.ru>
2024-11-27 16:45:25 +0300

    add makefile and start app

---
+++ Makefile
@@ -0,0 +1,2 @@
+run:
+	go run cmd/main.go --config=./config/local.yaml
--- cmd/main.go
+++ cmd/main.go
@@ -1,10 +1,16 @@
 package main
 
-import "tender/internal/config"
+import (
+	"log/slog"
+	"os"
+	"tender/internal/config"
+)
 
 func main() {
 	cfg := config.MustLoad()
 	// TODO: init config
+	log := slog.New(slog.NewTextHandler(os.Stdout, nil))
+	log.Info("starting app", slog.String("addr", cfg.ServerAddress))
 	// TODO: init app
 	// TODO: run app
 }
