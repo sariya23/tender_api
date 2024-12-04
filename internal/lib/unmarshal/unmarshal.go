c17f93ab5d691ec0a422135462712977c8623ffe
sariya <dayzwaitik123@yandex.ru>
2024-12-04 15:19:41 +0300

    add func to unmarshal edit request

--- internal/lib/unmarshal/unmarshal.go
+++ internal/lib/unmarshal/unmarshal.go
@@ -27,3 +27,23 @@ func CreateRequest(body []byte) (api.CreateTenderRequest, error) {
 
 	return req, nil
 }
+
+func EditRequest(body []byte) (api.EditTenderRequest, error) {
+	var req api.EditTenderRequest
+	err := json.Unmarshal(body, &req)
+
+	if err != nil {
+		var syntaxErr *json.SyntaxError
+		var typeErr *json.UnmarshalTypeError
+
+		if errors.As(err, &syntaxErr) {
+			return api.EditTenderRequest{}, fmt.Errorf("%s: %w", err.Error(), ErrSyntax)
+		} else if errors.As(err, &typeErr) {
+			return api.EditTenderRequest{}, fmt.Errorf("%s: %w", err.Error(), ErrType)
+		} else {
+			return api.EditTenderRequest{}, fmt.Errorf("%s: %w", err.Error(), ErrUnknown)
+		}
+	}
+
+	return req, nil
+}
