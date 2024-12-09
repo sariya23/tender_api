61dcd199974f2131f2e95b920eff190461850441
sariya <dayzwaitik123@yandex.ru>
2024-12-09 13:40:45 +0300

    delete checks from data layer

--- internal/repository/postgres/tender.go
+++ internal/repository/postgres/tender.go
@@ -2,7 +2,6 @@ package postgres
 
 import (
 	"context"
-	"errors"
 	"fmt"
 
 	"github.com/jackc/pgx/v5"
@@ -15,31 +14,7 @@ func (s *Storage) CreateTender(ctx context.Context, tender models.Tender) (creat
 
 	createQuery := `insert into tender(name, description, service_type, status, organization_id, creator_username)
 	values ($1, $2, $3, $4, $5, $6) returning name, description, service_type, organization_id, creator_username`
-	getEmployeeQuery := `select employee_id from employee where username = $1`
-	getOrgQuery := `select organization_id from organization where organization_id = $1`
 	createdTender = models.Tender{}
-
-	var employeeId int
-	rowEmployeeId := s.connection.QueryRow(ctx, getEmployeeQuery, tender.CreatorUsername)
-	err = rowEmployeeId.Scan(&employeeId)
-	if err != nil {
-		if errors.Is(err, pgx.ErrNoRows) {
-			return models.Tender{}, fmt.Errorf("%s: %w. Place = getEmployeeQuery", op, outerror.ErrEmployeeNotFound)
-		} else {
-			return models.Tender{}, fmt.Errorf("%s: %w. Place = getEmployeeQuery", op, err)
-		}
-	}
-
-	var orgId int
-	rowOrgId := s.connection.QueryRow(ctx, getOrgQuery, tender.OrganizationId)
-	err = rowOrgId.Scan(&orgId)
-	if err != nil {
-		if errors.Is(err, pgx.ErrNoRows) {
-			return models.Tender{}, fmt.Errorf("%s: %w. Place = getOrgQuery", op, outerror.ErrOrganizationNotFound)
-		} else {
-			return models.Tender{}, fmt.Errorf("%s: %w. Place = getOrgQuery", op, err)
-		}
-	}
 
 	tx, err := s.connection.BeginTx(ctx, pgx.TxOptions{})
 	if err != nil {
