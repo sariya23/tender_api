b89a1bbc417e1278502b8eaa6d84be4c1d77f7b8
sariya <dayzwaitik123@yandex.ru>
2024-12-14 13:13:25 +0300

    edit tender db

--- internal/repository/postgres/tender.go
+++ internal/repository/postgres/tender.go
@@ -171,15 +171,73 @@ func (s *Storage) GetEmployeeTenders(ctx context.Context, empl models.Employee) 
 func (s *Storage) EditTender(ctx context.Context, tenderId int, updateTender models.TenderToUpdate) (models.Tender, error) {
 	const op = "repository.postgres.tender.EditTender"
 
-	insertQuery := `insert into tender values (@name, @desc, @service_type, @status, @org_id, @username, @version)`
-	deactivatePrevVersion := `update tender set selected_version = $1 where tender_id = $2 and selected_version = $3`
+	insertQuery := `
+	insert into tender values (@name, @desc, @srv_type, @status, @org_id, @username, @version, @selected_version)
+	returning name, description, service_type, status, organization_id, creator_username`
 
 	oldTender, err := s.GetTenderById(ctx, tenderId)
 	if err != nil {
 		return models.Tender{}, fmt.Errorf("%s: %w", op, err)
 	}
 
-	args := pgx.NamedArgs{}
+	args := pgx.NamedArgs{"selected_version": true}
+
+	if newName := updateTender.TenderName; newName == nil {
+		args["name"] = oldTender.TenderName
+	} else {
+		args["name"] = newName
+	}
+
+	if newDesc := updateTender.Description; newDesc == nil {
+		args["desc"] = oldTender.Description
+	} else {
+		args["desc"] = newDesc
+	}
+
+	if newSrvType := updateTender.ServiceType; newSrvType == nil {
+		args["srv_type"] = oldTender.ServiceType
+	} else {
+		args["srv_type"] = newSrvType
+	}
+
+	if newStatus := updateTender.Status; newStatus == nil {
+		args["status"] = oldTender.Status
+	} else {
+		args["status"] = newStatus
+	}
+
+	if newOrgId := updateTender.OrganizationId; newOrgId == nil {
+		args["org_id"] = oldTender.OrganizationId
+	} else {
+		args["org_id"] = newOrgId
+	}
+
+	if newUsername := updateTender.CreatorUsername; newUsername == nil {
+		args["username"] = oldTender.CreatorUsername
+	} else {
+		args["username"] = newUsername
+	}
+
+	err = s.deactivateTenderVersion(ctx, tenderId)
+	if err != nil {
+		return models.Tender{}, fmt.Errorf("%s: %w", op, err)
+	}
+	var tender models.Tender
+
+	row := s.connection.QueryRow(ctx, insertQuery, args)
+	err = row.Scan(
+		&tender.TenderName,
+		&tender.Description,
+		&tender.ServiceType,
+		&tender.Status,
+		&tender.OrganizationId,
+		&tender.CreatorUsername,
+	)
+	if err != nil {
+		return models.Tender{}, fmt.Errorf("%s: %w", op, err)
+	}
+
+	return tender, nil
 }
 func (s *Storage) RollbackTender(ctx context.Context, tenderId int, toVersionRollback int) (models.Tender, error) {
 	panic("impl me")
@@ -230,4 +288,36 @@ func (s *Storage) GetLastInsertedTenderId(ctx context.Context) (int, error) {
 		return 0, fmt.Errorf("%s: %w", op, err)
 	}
 	return tenderId, nil
+}
+
+func (s *Storage) getLastTenderVersion(ctx context.Context, tenderId int) (int, error) {
+	const op = "repository.postgres.tender.getLastTenderVersion"
+	query := "select version from tender where tender_id = $1"
+	row := s.connection.QueryRow(ctx, query, tenderId)
+	var version int
+	err := row.Scan(&version)
+	if err != nil {
+		return 0, fmt.Errorf("%s: %w", op, err)
+	}
+	return version, nil
+}
+
+func (s *Storage) deactivateTenderVersion(ctx context.Context, tenderId int) error {
+	const op = "repository.postgres.tender.deactivateTenderVersion"
+	query := "update tender set selected_version = $1 where tender_id = $2"
+	_, err := s.connection.Exec(ctx, query, false, tenderId)
+	if err != nil {
+		return fmt.Errorf("%s: %w", op, err)
+	}
+	return nil
+}
+
+func (s *Storage) activateTenderVersion(ctx context.Context, tenderId int, version int) error {
+	const op = "repository.postgres.tender.activateTenderVersion"
+	query := "update tender set selected_version = $1 where tender_id = $2 and version = $3"
+	_, err := s.connection.Exec(ctx, query, true, tenderId, version)
+	if err != nil {
+		return fmt.Errorf("%s: %w", op, err)
+	}
+	return nil
 }
